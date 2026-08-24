38b2479a76a52dfb7cb20d011f33b8478f9d63ec
Swaroop Thorat <sthorat6663@gmail.com>
2026-08-24 23:19:26 +0530

    Pushed via PUSHkar

---
+++ LeetCode/Recursion/k-th-symbol-in-grammar/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        
+        int half=(int) Math.pow(2,n-2);
+
+        if(k<=half){
+            return kthGrammar(n-1,k);
+        }
+        else{
+            int prev=kthGrammar(n-1,k-half);
+            return prev==0?1:0;
+        }
+
+    }
+}
