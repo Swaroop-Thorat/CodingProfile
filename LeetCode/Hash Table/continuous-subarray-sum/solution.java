a94546f565651ef096f78543bb03b6fa0b20b638
Swaroop Thorat <sthorat6663@gmail.com>
2026-08-31 16:52:40 +0530

    Pushed via PUSHkar

---
+++ LeetCode/Hash Table/continuous-subarray-sum/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum%k) && i-map.get(sum%k)>1){
+                return true;
+            }
+            map.putIfAbsent(sum%k,i);
+        }
+        return false;
+    }
+}
