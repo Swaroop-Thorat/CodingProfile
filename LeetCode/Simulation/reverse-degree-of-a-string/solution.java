5344b8d037f8291963b77b88c8b069e1bc62b047
Swaroop Thorat <sthorat6663@gmail.com>
2026-09-20 17:47:04 +0530

    Pushed via PUSHkar

---
+++ LeetCode/Simulation/reverse-degree-of-a-string/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int[] arr=new int[26];
+        int num=26;
+        for(int i=0;i<26;i++){
+            arr[i]=(num--);
+        }
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+          sum+=(arr[s.charAt(i) - 'a']*(i+1));
+        }
+        return sum;
+    }
+}
